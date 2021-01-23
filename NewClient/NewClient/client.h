61d53470f9b282862c097c5b36cb262c9abdf9f2
Roy Carter <carterroy22@gmail.com>
2021-01-23 14:07:29 +0200

    Added macros

--- NewClient/NewClient/client.h
+++ NewClient/NewClient/client.h
@@ -18,6 +18,16 @@
 #define EQUAL_VAL '='
 #define POINT_VAL '.'
 #define END_LINE_VAL '\0'
+#define ADD_AND_INC(num1,num2) (num1+num2+1)
+#define SUB_AND_DEC(num1,num2) (num1-num2-1)
+#define SUB_AND_INC(num1,num2) (num1-num2+1)
+#define INC(num) (num+1)
+#define F_FAILED " Failed to open "
+#define OPEN_FAILED(name, func_name) printf(#name F_FAILED #func_name "\n");
+#define WRITE_F(file_name,modifier,value) fprintf(file_name,"%"#modifier,value);
+#define VAL_PRINT(val,modifier) printf(#val " = " "%"#modifier "\n",val);
+#include <stdio.h>
+
 
 typedef struct lua_line{
 	char * name;
