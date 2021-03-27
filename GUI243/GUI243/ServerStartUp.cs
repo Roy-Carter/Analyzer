aaaf121bb73f4f8a8c1a315eabfcd445cc9f51c7
Roy Carter <carterroy22@gmail.com>
2021-03-28 00:45:52 +0300

    C# GUI

---
+++ GUI243/GUI243/ServerStartUp.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace ServerOn
+{
+    class Server
+    {
+        public static void ExecuteServer()
+        {
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5400);//switch the port
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            listenSocket.Bind(ipPoint);
+            listenSocket.Listen(2);
+            MessageBox.Show("Waiting connection ... ");
+            Socket clientSocket = listenSocket.Accept();
+            bool flag = false;
+            byte[] buffer = new Byte[1024];
+            while (!flag)
+            {
+                flag = ReceiveFile(clientSocket, buffer);
+            }
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+            // byte[] message = Encoding.ASCII.GetBytes("Test Server");
+            // clientSocket.Send(message);
+
+        }
+        public static bool ReceiveFile(Socket clientSocket, byte[] buffer) // need to check about the retval thingy , not fully okay
+        {
+            bool retval = false;
+            string data = null;
+            int numByte;
+            string folder = @"C:\Users\Roy\source\repos\GUI243\GUI243\";
+            byte[] name = new Byte[8];
+
+            numByte = clientSocket.Receive(name);
+            string fileName = Encoding.ASCII.GetString(name, 0, 7);
+            string fullPath = folder + fileName;
+            do
+            {
+                numByte = clientSocket.Receive(buffer);
+                if (numByte > 0)
+                {
+                    data += Encoding.ASCII.GetString(buffer, 0, numByte); // check about +=
+
+                    File.WriteAllText(fullPath, data);
+                }
+            } while (numByte > 0);
+            MessageBox.Show("File completely received");
+            retval = true;
+            return retval;
+        }
+
+    }
+
+}
+
+
---
+++ GUI243/GUI243/descriptor.lua
@@ -0,0 +1,6 @@
+p_type uint8 0
+flag uint8 0
+seq uint8 0
+my_data_req uint16 1
+my_data_res uint16 2
+check_it uint8 2
