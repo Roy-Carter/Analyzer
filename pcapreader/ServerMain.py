777375b45ee3ff80f5bf84548d50b58e5b53e597
Roy Carter <carterroy22@gmail.com>
2021-03-29 18:22:49 +0300

    Added python client callout

--- pcapreader/ServerMain.py
+++ pcapreader/ServerMain.py
@@ -1,9 +1,11 @@
 from Server import *
+from ConnectToGUI import *
 
 
 def main():
     server = Server(HOST, PORT)
     ret_val = server.handle_server()
+    client_program() # check about the i/o problem
 
 
 if __name__ == '__main__':
